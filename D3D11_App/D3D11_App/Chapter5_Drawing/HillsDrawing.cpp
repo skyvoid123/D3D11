f0cd46a59554a064e7ed8c4344fce5a2f5e34dd2
skyvoid <skyvoid@qq.com>
2016-09-27 18:53:37 +0800

    Hills Drawing

---
+++ D3D11_App/D3D11_App/Chapter5_Drawing/HillsDrawing.cpp
@@ -0,0 +1,332 @@
+#include "d3dApp.h"
+#include "d3dx11effect.h"
+#include "GeometryGenerator.h"
+#include "MathHelper.h"
+
+struct Vertex
+{
+	XMFLOAT3 Pos;
+	XMFLOAT4 Color;
+};
+
+class HillsApp : public D3DApp
+{
+public:
+	HillsApp(HINSTANCE hInstance);
+	~HillsApp();
+
+	virtual bool Init() override;
+	virtual void OnResize() override;
+	virtual void UpdateScene(float dt) override;
+	virtual void DrawScene() override;
+
+	virtual void OnMouseDown(WPARAM btn_state, int x, int y) override;
+	virtual void OnMouseUp(WPARAM btn_state, int x, int y) override;
+	virtual void OnMouseMove(WPARAM btn_state, int x, int y) override;
+
+private:
+	float GetHeight(float x, float z) const;
+	void BuildGeometryBuffers();
+	void BuildFX();
+	void BuildVertexLayout();
+
+private:
+	ID3D11Buffer* vertex_buffer_;
+	ID3D11Buffer* index_buffer_;
+
+	ID3DX11Effect* fx_;
+	ID3DX11EffectTechnique* technique_;
+	ID3DX11EffectMatrixVariable* fx_WVP_;
+
+	ID3D11InputLayout* input_layout_;
+
+	// Define transformations from local spaces to world space.
+	XMFLOAT4X4 world_;
+	XMFLOAT4X4 view_;
+	XMFLOAT4X4 proj_;
+	
+	UINT grid_index_count_;
+
+	float theta_;
+	float phi_;
+	float radius_;
+
+	POINT last_mouse_pos_;
+};
+
+HillsApp::HillsApp(HINSTANCE hInstance)
+	: D3DApp(hInstance)
+	, vertex_buffer_(nullptr)
+	, index_buffer_(nullptr)
+	, fx_(nullptr)
+	, technique_(nullptr)
+	, fx_WVP_(nullptr)
+	, input_layout_(nullptr)
+	, grid_index_count_(0)
+	, theta_(1.5f * MathHelper::Pi)
+	, phi_(0.1f * MathHelper::Pi)
+	, radius_(200.0f)
+{
+	main_wnd_caption_ = L"Hills Demo";
+
+	last_mouse_pos_.x = 0;
+	last_mouse_pos_.y = 0;
+
+	XMMATRIX I = XMMatrixIdentity();
+	XMStoreFloat4x4(&world_, I);
+	XMStoreFloat4x4(&view_, I);
+	XMStoreFloat4x4(&proj_, I);
+}
+
+HillsApp::~HillsApp()
+{
+	ReleaseCOM(vertex_buffer_);
+	ReleaseCOM(index_buffer_);
+	ReleaseCOM(fx_);
+	ReleaseCOM(input_layout_);
+}
+
+bool HillsApp::Init()
+{
+	if (!D3DApp::Init())
+	{
+		return false;
+	}
+	BuildGeometryBuffers();
+	BuildFX();
+	BuildVertexLayout();
+	
+	return true;
+}
+
+void HillsApp::BuildGeometryBuffers()
+{
+	GeometryGenerator::MeshData grid;
+	GeometryGenerator geometry_generator;
+
+	geometry_generator.CreateGrid(160.0f, 160.0f, 50, 50, grid);
+
+	grid_index_count_ = grid.Indices.size();
+
+	// Extract the vertex elements we are interested and apply the height function to
+	// each vertex.  In addition, color the vertices based on their height so we have
+	// sandy looking beaches, grassy low hills, and snow mountain peaks.
+	std::vector<Vertex> v(grid.Vertices.size());
+
+	for (int i = 0; i < grid.Vertices.size(); ++i)
+	{
+		XMFLOAT3 pos = grid.Vertices[i].Position;
+		pos.y = GetHeight(pos.x, pos.z);
+		v[i].Pos = pos;
+
+		// Color the vertex based on its height.
+		if (pos.y < -10.f)
+		{
+			// Sandy beach color.
+			v[i].Color = XMFLOAT4(1.f, 0.96f, 0.62f, 1.0f);
+		}
+		else if (pos.y < 5.f)
+		{
+			// Light yellow-green.
+			v[i].Color = XMFLOAT4(0.48f, 0.77f, 0.46f, 1.0f);
+		}
+		else if (pos.y < 12.f)
+		{
+			// Dark yellow-green.
+			v[i].Color = XMFLOAT4(0.1f, 0.48f, 0.19f, 1.0f);
+		}
+		else if (pos.y < 20.f)
+		{
+			// Dark brown.
+			v[i].Color = XMFLOAT4(0.45f, 0.39f, 0.34f, 1.0f);
+		}
+		else
+		{
+			// White snow.
+			v[i].Color = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
+		}
+	}
+
+	D3D11_BUFFER_DESC vbd;
+	vbd.Usage = D3D11_USAGE_IMMUTABLE;
+	vbd.ByteWidth = sizeof(Vertex) * grid.Vertices.size();
+	vbd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	vbd.CPUAccessFlags = 0;
+	vbd.MiscFlags = 0;
+	
+	D3D11_SUBRESOURCE_DATA vinitData;
+	vinitData.pSysMem = &v[0];
+	
+	HR(d3d_device_->CreateBuffer(&vbd, &vinitData, &vertex_buffer_));
+
+	// Pack the indices of all the meshes into one index buffer.
+	D3D11_BUFFER_DESC ibd;
+	ibd.Usage = D3D11_USAGE_IMMUTABLE;
+	ibd.ByteWidth = sizeof(UINT) * grid_index_count_;
+	ibd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	ibd.CPUAccessFlags = 0;
+	ibd.MiscFlags = 0;
+
+	D3D11_SUBRESOURCE_DATA iinitData;
+	iinitData.pSysMem = &grid.Indices[0];
+
+	HR(d3d_device_->CreateBuffer(&ibd, &iinitData, &index_buffer_));
+}
+
+void HillsApp::BuildFX()
+{
+	std::ifstream fin("fx/hills.fxo", std::ios::binary);
+
+	fin.seekg(0, std::ios_base::end);
+	int size = (int)fin.tellg();
+	fin.seekg(0, std::ios_base::beg);
+	
+	std::vector<char> compiled_shader(size);
+
+	fin.read(&compiled_shader[0], size);
+	fin.close();
+
+	HR(D3DX11CreateEffectFromMemory(&compiled_shader[0], size,
+		0, d3d_device_, &fx_));
+
+	technique_ = fx_->GetTechniqueByName("ColorTech");
+	fx_WVP_ = fx_->GetVariableByName("gWorldViewProj")->AsMatrix();
+}
+
+void HillsApp::BuildVertexLayout()
+{
+	// Create the vertex input layout.
+	D3D11_INPUT_ELEMENT_DESC vertex_desc[] = {
+		{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
+		{"COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0}
+	};
+
+	// Create the input layout
+	D3DX11_PASS_DESC pass_desc;
+	technique_->GetPassByIndex(0)->GetDesc(&pass_desc);
+	HR(d3d_device_->CreateInputLayout(vertex_desc, 2, pass_desc.pIAInputSignature,
+		pass_desc.IAInputSignatureSize, &input_layout_));
+}
+
+void HillsApp::OnResize()
+{
+	D3DApp::OnResize();
+
+	XMMATRIX p = XMMatrixPerspectiveFovLH(0.25f * MathHelper::Pi, AspectRatio(), 1.0f, 1000.0f);
+	XMStoreFloat4x4(&proj_, p);
+}
+
+void HillsApp::UpdateScene(float dt)
+{
+	// Convert Spherical to Cartesian coordinates.
+	float x = radius_*sinf(phi_)*cosf(theta_);
+	float z = radius_*sinf(phi_)*sinf(theta_);
+	float y = radius_*cosf(phi_);
+
+	// Build the view matrix.
+	XMVECTOR pos = XMVectorSet(x, y, z, 1.0f);
+	XMVECTOR target = XMVectorZero();
+	XMVECTOR up = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+
+	XMMATRIX v = XMMatrixLookAtLH(pos, target, up);
+	XMStoreFloat4x4(&view_, v);
+}
+
+void HillsApp::DrawScene()
+{
+	d3d_context_->ClearRenderTargetView(render_target_view_, (const float*)&Colors::LightSteelBlue);
+	d3d_context_->ClearDepthStencilView(depth_stencil_view_, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
+
+	d3d_context_->IASetInputLayout(input_layout_);
+	d3d_context_->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	UINT stride = sizeof(Vertex);
+	UINT off = 0;
+	d3d_context_->IASetVertexBuffers(0, 1, &vertex_buffer_, &stride, &off);
+	d3d_context_->IASetIndexBuffer(index_buffer_, DXGI_FORMAT_R32_UINT, 0);
+
+	// Set constants
+	XMMATRIX world = XMLoadFloat4x4(&world_);
+	XMMATRIX view = XMLoadFloat4x4(&view_);
+	XMMATRIX proj = XMLoadFloat4x4(&proj_);
+	XMMATRIX wvp = world * view * proj;
+
+	D3DX11_TECHNIQUE_DESC tech_desc;
+	technique_->GetDesc(&tech_desc);
+	for (int p = 0; p < tech_desc.Passes; ++p)
+	{
+		// Draw the grid.
+		fx_WVP_->SetMatrix((float*)&wvp);
+		technique_->GetPassByIndex(p)->Apply(0, d3d_context_);
+		d3d_context_->DrawIndexed(grid_index_count_, 0, 0);
+	}
+
+	HR(swap_chain_->Present(0, 0));
+}
+
+void HillsApp::OnMouseDown(WPARAM btn_state, int x, int y)
+{
+	last_mouse_pos_.x = x;
+	last_mouse_pos_.y = y;
+
+	SetCapture(main_wnd_);
+}
+
+void HillsApp::OnMouseUp(WPARAM btn_state, int x, int y)
+{
+	ReleaseCapture();
+}
+
+void HillsApp::OnMouseMove(WPARAM btn_state, int x, int y)
+{
+	if ((btn_state & MK_LBUTTON) != 0)
+	{
+		// Make each pixel correspond to a quarter of a degree.
+		float dx = XMConvertToRadians(0.25f * (float)(x - last_mouse_pos_.x));
+		float dy = XMConvertToRadians(0.25f * (float)(x - last_mouse_pos_.y));
+
+		// Update angles based on input to orbit camera around box.
+		theta_ += dx;
+		phi_ += dy;
+
+		// Restrict the angle mPhi.
+		phi_ = MathHelper::Clamp(phi_, 0.1f, MathHelper::Pi - 0.1f);
+	}
+	else if ((btn_state & MK_RBUTTON) != 0)
+	{
+		// Make each pixel correspond to 0.2 unit in the scene.
+		float dx = 0.2f * (float)(x - last_mouse_pos_.x);
+		float dy = 0.2f * (float)(y - last_mouse_pos_.y);
+
+		// Update the camera radius based on input.
+		radius_ += dx - dy;
+
+		// Restrict the radius.
+		radius_ = MathHelper::Clamp(radius_, 50.0f, 500.0f);
+	}
+
+	last_mouse_pos_.x = x;
+	last_mouse_pos_.y = y;
+}
+
+float HillsApp::GetHeight(float x, float z) const
+{
+	return 0.3f * (z * sinf(0.1f * x) + x * cosf(0.1f * z));
+}
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
+{
+	// Enable run-time memory check for debug builds.
+#if defined(DEBUG) | defined(_DEBUG)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+	HillsApp theApp(hInstance);
+
+	if (!theApp.Init())
+	{
+		return 0;
+	}
+
+	return theApp.Run();
+}
---
+++ D3D11_App/D3D11_App/Common/GeometryGenerator.cpp
@@ -0,0 +1,126 @@
+#include "GeometryGenerator.h"
+#include "MathHelper.h"
+
+void GeometryGenerator::CreateBox(float width, float height, float depth, MeshData& meshData)
+{
+	// Create Vertex
+	Vertex v[24];
+	//std::vector<Vertex>v;
+	//v.resize(24);
+
+	float w2 = 0.5f * width;
+	float h2 = 0.5f * height;
+	float d2 = 0.5f * depth;
+
+	// Fill in the front face vertex data.
+	v[0] = { -w2, -h2, -d2, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f };
+	v[1] = { -w2, h2, -d2, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f };
+	v[2] = { w2, h2, -d2, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f };
+	v[3] = { w2, -h2, -d2, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f };
+
+	// Fill in the back face vertex data.
+	v[4] = { -w2, -h2, +d2, 0.0f, 0.0f, 1.0f, -1.0f, 0.0f, 0.0f, 1.0f, 1.0f };
+	v[5] = { +w2, -h2, +d2, 0.0f, 0.0f, 1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f };
+	v[6] = { +w2, +h2, +d2, 0.0f, 0.0f, 1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f };
+	v[7] = { -w2, +h2, +d2, 0.0f, 0.0f, 1.0f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f };
+
+	// Fill in the top face vertex data.
+	v[8] = { -w2, +h2, -d2, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f };
+	v[9] = { -w2, +h2, +d2, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f };
+	v[10] = { +w2, +h2, +d2, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f };
+	v[11] = { +w2, +h2, -d2, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f };
+
+	// Fill in the bottom face vertex data.
+	v[12] = { -w2, -h2, -d2, 0.0f, -1.0f, 0.0f, -1.0f, 0.0f, 0.0f, 1.0f, 1.0f };
+	v[13] = { +w2, -h2, -d2, 0.0f, -1.0f, 0.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f };
+	v[14] = { +w2, -h2, +d2, 0.0f, -1.0f, 0.0f, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f };
+	v[15] = { -w2, -h2, +d2, 0.0f, -1.0f, 0.0f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f };
+
+	// Fill in the left face vertex data.
+	v[16] = { -w2, -h2, +d2, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f };
+	v[17] = { -w2, +h2, +d2, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f };
+	v[18] = { -w2, +h2, -d2, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f };
+	v[19] = { -w2, -h2, -d2, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, 1.0f, 1.0f };
+
+	// Fill in the right face vertex data.
+	v[20] = { +w2, -h2, -d2, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f };
+	v[21] = { +w2, +h2, -d2, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f };
+	v[22] = { +w2, +h2, +d2, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f };
+	v[23] = { +w2, -h2, +d2, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f };
+
+	meshData.Vertices.assign(&v[0], &v[24]);
+
+	// Create the indices.
+	UINT i[36] = {
+		// front face
+		0, 1, 2,
+		0, 2, 3,
+		// back face
+		4, 5, 6,
+		4, 6, 7,
+		// top face
+		8, 9, 10,
+		8, 10, 11,
+		// bottom face
+		12, 13, 14,
+		12, 14, 15,
+		// left face
+		16, 17, 18,
+		16, 18, 19,
+		// right face
+		20, 21, 22,
+		20, 22, 23
+	};
+
+	meshData.Indices.assign(&i[0], &i[36]);
+}
+
+
+void GeometryGenerator::CreateGrid(float width, float depth, UINT m, UINT n, MeshData& meshData)
+{
+	UINT vertex_count = m * n;
+	UINT face_count = (m - 1) * (n - 1) * 2;
+
+	// Create the vertices.
+	float dx = width / (n - 1);
+	float dz = depth / (m - 1);
+
+	float du = 1.0f / (n - 1);
+	float dv = 1.0f / (m - 1);
+
+	meshData.Vertices.resize(vertex_count);
+
+	for (UINT z = 0; z < m; ++z)
+	{
+		float current_depth = depth / 2 - z * dz;
+		for (UINT x = 0; x < n; ++x)
+		{
+			float current_width = -width / 2 + x * dx;
+			meshData.Vertices[z * n + x].Position = XMFLOAT3(current_width, 0.0f, current_depth);
+			meshData.Vertices[z * n + x].Normal = XMFLOAT3(0.0f, 1.0f, 0.0f);
+			meshData.Vertices[z * n + x].TangentU = XMFLOAT3(1.0f, 0.0f, 0.0f);
+			meshData.Vertices[z * n + x].TexC.x = (float)x / n;
+			meshData.Vertices[z * n + x].TexC.y = (float)z / m;
+		}
+	}
+
+	// Create the indices.
+	meshData.Indices.resize(face_count * 3);
+	UINT current_index = 0;
+
+	for (UINT z = 0; z < m - 1; ++z)
+	{
+		for (UINT x = 0; x < n - 1; ++x)
+		{
+			meshData.Indices[current_index] = z * n + x;
+			meshData.Indices[current_index + 1] = z * n + x + 1;
+			meshData.Indices[current_index + 2] = z * (n + 1) + x;
+
+			meshData.Indices[current_index + 3] = z * n + x;
+			meshData.Indices[current_index + 4] = z * (n + 1) + x;
+			meshData.Indices[current_index + 4] = z * (n + 1) + x + 1;
+
+			current_index += 6;
+		}
+	}
+}
